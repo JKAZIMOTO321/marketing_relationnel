96ae8bc12abac76271e4b134a4a74789e788c6c6
Josue Kazimoto <kazimotojosue@gmail.com>
2026-01-29 09:45:15 +0200

    Refactor la méthode construire_graphe pour utiliser les attributs de classe relations et achats, améliorant ainsi la lisibilité et l'organisation du code.

--- services/graphes_service.py
+++ services/graphes_service.py
@@ -1,27 +1,25 @@
+from database import DatabaseManager as db
 class GraphService:
-    def __init__(self, relations, achats):
-        self.relations = relations
-        self.achats = achats
+    def __init__(self):
+        self.relations = db.get_relations()
+        self.achats = db.get_tot_achats
         self.graphe = {}
     
     def construire_graphe(self, taux_direct=0.05):
-        relations = self.get_relations()
-        achats_totaux = self.get_tot_achats()
-        graphe = {}
-
-        for relation in relations:
+        for relation in self.relations:
             parrain = relation["parrainID"]
             filleul = relation["filleulID"]
-            total = achats_totaux.get(filleul,0)
+            total = self.achats.get(filleul,0)
             poids = total*taux_direct
             # Ajout du parrain dans le graphe s'il n'y est pas
-            if parrain not in graphe:
-                graphe[parrain]=[]
-            graphe[parrain].append((filleul, poids))
+            if parrain not in self.graphe:
+                self.graphe[parrain]=[]
+            
+            self.graphe[parrain].append((filleul, poids))
 
             # vu que le filleul doit etre aussi un sommet
-            if filleul not in graphe:
-                graphe[filleul] = []
-        return graphe
+            if filleul not in self.graphe:
+                self.graphe[filleul] = []
+        return self.graphe
 
     
