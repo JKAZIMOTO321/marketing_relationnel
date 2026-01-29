12da250acee206edb4a1ad399e4e86fddb08204c
Josue Kazimoto <kazimotojosue@gmail.com>
2026-01-29 09:33:32 +0200

    Déplacement de la méthode construire_graphe de DatabaseManager vers GraphService pour une meilleure organisation et encapsulation de la logique de graphe.

--- database/database_manager.py
+++ database/database_manager.py
@@ -106,25 +106,7 @@ class DatabaseManager():
         query = "SELECT ClientID, SUM(Montant) AS total FROM Achats GROUP BY ClientID;"
         return self.requetes_select(requette=query)
     
-    def construire_graphe(self, taux_direct=0.05):
-        relations = self.get_relations()
-        achats_totaux = self.get_tot_achats()
-        graphe = {}
-
-        for relation in relations:
-            parrain = relation["parrainID"]
-            filleul = relation["filleulID"]
-            total = achats_totaux.get(filleul,0)
-            poids = total*taux_direct
-            # Ajout du parrain dans le graphe s'il n'y est pas
-            if parrain not in graphe:
-                graphe[parrain]=[]
-            graphe[parrain].append((filleul, poids))
-
-            # vu que le filleul doit etre aussi un sommet
-            if filleul not in graphe:
-                graphe[filleul] = []
-        return graphe
+    
             
 
         
--- services/graphes_service.py
+++ services/graphes_service.py
@@ -4,9 +4,24 @@ class GraphService:
         self.achats = achats
         self.graphe = {}
     
-    def construireGraphe(self):
-        for relation in self.relations:
-            return
-        pass
+    def construire_graphe(self, taux_direct=0.05):
+        relations = self.get_relations()
+        achats_totaux = self.get_tot_achats()
+        graphe = {}
+
+        for relation in relations:
+            parrain = relation["parrainID"]
+            filleul = relation["filleulID"]
+            total = achats_totaux.get(filleul,0)
+            poids = total*taux_direct
+            # Ajout du parrain dans le graphe s'il n'y est pas
+            if parrain not in graphe:
+                graphe[parrain]=[]
+            graphe[parrain].append((filleul, poids))
+
+            # vu que le filleul doit etre aussi un sommet
+            if filleul not in graphe:
+                graphe[filleul] = []
+        return graphe
 
     
