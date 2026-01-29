a61c21ad5b184fa4d3a71d2d6e28c5581bc15c37
Josue Kazimoto <kazimotojosue@gmail.com>
2026-01-29 11:44:09 +0200

    Ajout de l'initialisation de GraphService dans CommissionService et création de l'attribut graphe.

--- services/commissions_service.py
+++ services/commissions_service.py
@@ -1,5 +1,10 @@
-from services import GraphService
+from graphes_service import GraphService
 
 class CommissionService:
     def __init__(self):
+        self.graph_service = GraphService()
+        self.graphe = self.graph_service.graphe
+
+    def dfs_commission(self):
         pass
+
