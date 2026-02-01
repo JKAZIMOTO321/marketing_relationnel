ba2fbeee1f3989b1566c192bffdb5080e22ff5e0
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-01 14:21:12 +0200

    Ajout de la méthode dfs_commission dans CommissionService pour calculer les commissions et mise à jour de l'initialisation des attributs.

--- services/commissions_service.py
+++ services/commissions_service.py
@@ -4,7 +4,11 @@ class CommissionService:
     def __init__(self):
         self.graph_service = GraphService()
         self.graphe = self.graph_service.graphe
+        self.achats_totaux = self.graph_service.achats
 
-    def dfs_commission(self):
-        pass
+    def dfs_commission(self, graphe, achats_totaux, client_courant,niveau, visited):
+        commission = 0
+        visited.add(client_courant)
+
+        taux = 0.05 if niveau == 1 else 0.01
 
