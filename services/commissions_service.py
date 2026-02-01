e68358bc3db5a2cc2a2ca654b014eb806f4aea4d
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-01 15:30:51 +0200

    Réorganisation de la méthode dfs_commission pour améliorer la lisibilité et la structure du code.

--- services/commissions_service.py
+++ services/commissions_service.py
@@ -9,6 +9,20 @@ class CommissionService:
     def dfs_commission(self, graphe, achats_totaux, client_courant,niveau, visited):
         commission = 0
         visited.add(client_courant)
-
         taux = 0.05 if niveau == 1 else 0.01
+        for filleul, _ in graphe.get(client_courant,[]):
+            if filleul not in visited:
+                total_achat = achats_totaux.get(filleul,0)
+                gain = total_achat*taux
+                commission += gain
+
+                # recursivité
+                commission += self.dfs_commission(
+                    graphe,
+                    achats_totaux,
+                    filleul,
+                    niveau+1,
+                    visited
+                )
+        return commission
 
