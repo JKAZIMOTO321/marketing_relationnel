442d484c29a637f26da4131c03b5fb6877787879
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-03 11:27:36 +0200

    Ajout de l'initialisation des services GraphService et CommissionService dans main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from services import GraphService, CommissionService
+
+grapheserv = GraphService()
+graphe = grapheserv.graphe
+commissionn = CommissionService()
+commissionJK = commissionn.get_commission_total(parrainID=1)
+print(graphe)
