98c2bfd897d33fa9ad7fe14c6ff2b39a52cf77fc
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-10 11:26:27 +0200

    mis en place du point d'entrée de l'application

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from services import GraphService, CommissionService
+import sys
+from PyQt5.QtWidgets import QApplication
+from servicesGUI.main_window import MainWindow
+from servicesGUI.relations_window import RelationsPage
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
