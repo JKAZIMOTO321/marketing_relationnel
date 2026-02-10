295901845a955a4ea606481759ef3626ea01b2d7
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-10 12:06:04 +0200

    mis en place de lafonctions pour charger les clients dans le combo box au  cas où le client aurait un parrain

--- servicesGUI/client_window.py
+++ servicesGUI/client_window.py
@@ -1,8 +1,27 @@
 from PyQt5.QtWidgets import QWidget
 from gui.ui_clientWindow import Ui_Form
+from services.client_services import ClientsService
 
 class ClientPage(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.clientService = ClientsService()
+        self.chargerParrainsComboBox(self.ui.comboBox_AddParrain)
+
+
+    def chargerParrainsComboBox(self, ComboBox):
+        ComboBox.clear()
+        ComboBox.addItem("Aucun Parrain", None)
+        #depuis la BDD
+        clients = self.clientService.recupererNomId()
+        for client in clients:
+            client_id = client["ClientID"]
+            clientNom = client["ClientName"]
+            ComboBox.addItem(
+                clientNom,
+                client_id
+            )
+
+        
