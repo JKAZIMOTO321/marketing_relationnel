a733fd10a433fc39fa75f4ea5dc8519773553188
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-10 11:22:52 +0200

    ajouts des autres fenetres dans la fenetre principale

--- servicesGUI/main_window.py
+++ servicesGUI/main_window.py
@@ -1,10 +1,39 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from gui.ui_mainWindow import Ui_MainWindow
+from .achat_window import AchatsPage
+from .client_window import ClientPage
+from .commission_window import CommissionPage
+from .graphe_window import GraphePage
+from .relations_window import RelationsPage
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        #instanciation des pages
+        self.fenetres = {
+            "achats" : AchatsPage(),
+            "clients" : ClientPage(),
+            "commissions": CommissionPage(),
+            "graphe" : GraphePage(),
+            "relations" : RelationsPage(),
+        }
+        #Ajout de ces fenetre au stackedWidget du mainWindow pour permettre la navigation
+        for fenetre in self.fenetres.values():
+            self.ui.stackedWidget.addWidget(fenetre)
+
+        #Mettre les pages dans le menu
+        self.ui.actionAchats.triggered.connect(lambda: self.afficherFenetre("achats"))
+        self.ui.actionClients.triggered.connect(lambda: self.afficherFenetre("clients"))
+        self.ui.actionGraphe.triggered.connect(lambda : self.afficherFenetre("graphe"))
+        self.ui.actionRelations.triggered.connect(lambda: self.afficherFenetre("relations"))
+
+    def afficherFenetre(self, nomFenetre):
+        self.ui.stackedWidget.setCurrentWidget(self.fenetres[nomFenetre])
+
+
+        
 
         
