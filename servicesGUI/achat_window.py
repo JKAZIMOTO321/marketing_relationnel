2ecddce3fcf28ce74e2d6068cd7ed6b2fcb22a93
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-12 17:00:07 +0200

    ajout de la fonctionnalite pour enregistrer un achat

--- servicesGUI/achat_window.py
+++ servicesGUI/achat_window.py
@@ -1,10 +1,32 @@
 from PyQt5.QtWidgets import QWidget
 from gui.ui_achatWindow import Ui_Form
-from utilitaires import ajusterColonnesDansTables
+from services.achats_services import AchatsServices
+from .utilitaires import (ajusterColonnesDansTables, 
+                          chargerClientsDansComboBox,
+                          afficher_alerte,
+                          afficher_information)
 class AchatsPage(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.tableaux = [self.ui.tableWidget]
+        self.achatsS = AchatsServices()
         ajusterColonnesDansTables(listTables=self.tableaux)
+        self.chargerCB()
+        self.ui.btnEnreigistrer.clicked.connect(self.enregistrerAchat)
+
+    def chargerCB(self):
+        chargerClientsDansComboBox(self.ui.comboBox_NomClient)
+
+    def enregistrerAchat(self):
+        try:
+            idClient = int(self.ui.comboBox_NomClient.currentData())
+            montant = float(self.ui.doubleSpinBoxMontant.text())
+            self.achatsS.enregistrerAchat(idClient=idClient, Montant=montant)
+            afficher_information(message="Succès !")
+        except Exception as e:
+            afficher_alerte(message=f"Erreur : {e}")
+
+
+    
