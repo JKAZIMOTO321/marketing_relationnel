3890e6165f6e4bd86fcb6a6af07cf2450d6f0721
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-12 16:21:09 +0200

    ajout de la logique pour organiser les colonnes dans la table

--- servicesGUI/achat_window.py
+++ servicesGUI/achat_window.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import QWidget
 from gui.ui_achatWindow import Ui_Form
-
+from utilitaires import ajusterColonnesDansTables
 class AchatsPage(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.tableaux = [self.ui.tableWidget]
+        ajusterColonnesDansTables(listTables=self.tableaux)
