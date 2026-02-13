31257a49d5f8401c34bae9e20f9f37173b975367
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-13 12:30:05 +0200

    ajout de la fonction relationCreeraitCycle qui va pouvoir m'aider pour empecher qu;un filleul ne parrain un des ses parrains eloigné

--- services/relations_services.py
+++ services/relations_services.py
@@ -1,9 +1,28 @@
 from database.database_manager import DatabaseManager
+from .graphes_service import GraphService
 
 class RelationService:
     def __init__(self):
         self.db = DatabaseManager()
+        self.grapheService = GraphService()
 
     def get_data_tableau(self):
         data = self.db.get_relations_ClientName()
         return data
+    
+    def relationCreeraitCycle(self, parrainID, filleulID):
+        graphe = self.grapheService.construire_graphe()
+        visited = set()
+
+        def dfs(noeud):
+            if noeud == parrainID:
+                return True
+            visited.add(noeud)
+
+            for voisin in graphe.get(noeud, []):
+                if voisin not in visited:
+                    if dfs(voisin):
+                        return True
+            return False
+
+        return dfs(filleulID)
