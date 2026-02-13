37480a0dfbb7e0fa84b88e8b296c5369b6b3ba3e
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-13 11:04:51 +0200

    ajout de relationService pour gerer ce service

---
+++ services/relations_services.py
@@ -0,0 +1,8 @@
+from database.database_manager import DatabaseManager
+
+class RelationService:
+    def __init__(self):
+        self.db = DatabaseManager()
+
+    def get_data_tableau(self):
+        pass
