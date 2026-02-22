22b33b22635bb8e7271492da51fea74726ad5b67
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-22 12:55:14 +0200

    ajout de la fonctions charger commissions pour charger les donnees depuis le service des commissions et les afficher dans les tables via la fonction remplir tables

--- servicesGUI/commission_window.py
+++ servicesGUI/commission_window.py
@@ -1,14 +1,30 @@
 from PyQt5.QtWidgets import QWidget, QTableWidgetItem
 from gui.ui_commissionWindow import Ui_Form
+from services.commissions_service import CommissionService
 from utilitaires import ajusterColonnesDansTables
 class CommissionPage(QWidget):
     def __init__(self, idClient=None):
         super().__init__()
         self.ui = Ui_Form()
         self.idClient = idClient
+        self.service = CommissionService()
         self.ui.setupUi(self)
         tables = [self.ui.tableFilleulDirect, self.ui.tableFilleulIndirect]
         ajusterColonnesDansTables(tables)
+
+    def charger_commissions(self):
+        directs, indirects = self.service.get_commissions_details(self.idClient)
+
+        self.remplir_table(self.ui.tableFilleulDirect, directs)
+        self.remplir_table(self.ui.tableFilleulIndirect, indirects)
+
+        total_direct = sum(c[2] for c in directs)
+        total_indirect = sum(c[2] for c in indirects)
+        total_general = total_direct + total_indirect
+
+        self.ui.lineEdit_Tot_Comm_direct.setText(str(total_direct))
+        self.ui.lineEdit_Tot_Comm_indirect.setText(str(total_indirect))
+        self.ui.lblTotalCommissions.setText(str(total_general))
 
     def remplir_table(self, table, data):
         table.setRowCount(len(data))
