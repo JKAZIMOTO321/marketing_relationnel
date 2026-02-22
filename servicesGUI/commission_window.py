a674c45f49bc2c4e36394a49becbe05bf328568e
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-22 12:08:22 +0200

    ajout de la gestion de l'ID client dans la classe CommissionPage et ajustement des colonnes des tables

--- servicesGUI/commission_window.py
+++ servicesGUI/commission_window.py
@@ -1,8 +1,13 @@
 from PyQt5.QtWidgets import QWidget
 from gui.ui_commissionWindow import Ui_Form
-
+from utilitaires import ajusterColonnesDansTables
 class CommissionPage(QWidget):
-    def __init__(self):
+    def __init__(self, idClient=None):
         super().__init__()
         self.ui = Ui_Form()
+        self.idClient = idClient
         self.ui.setupUi(self)
+        tables = [self.ui.tableFilleulDirect, self.ui.tableFilleulIndirect]
+        ajusterColonnesDansTables(tables)
+
+    
