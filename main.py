a08535b383c38e9f3b3725efb1ddc8ae7733b653
Josue Kazimoto <kazimotojosue@gmail.com>
2026-02-23 18:33:56 +0200

    suppression des lignes sans importance dans le fichier d'entré dans le programme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from servicesGUI.main_window import MainWindow
-from servicesGUI.relations_window import RelationsPage
-
 
 app = QApplication(sys.argv)
 window = MainWindow()
